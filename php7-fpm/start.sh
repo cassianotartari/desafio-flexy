49a9aac519709a32ed394bc4f7c1d20857b88fd8
Cassiano Tartari <cassianotartari@gmail.com>
2017-09-04 11:11:32 -0300

    Sonata Admin e User funcionando.

---
+++ php7-fpm/start.sh
@@ -0,0 +1,2 @@
+php app/console doctrine:schema:update --dump-sql
+php app/console fos:user:create admin cassianotartari@gmail.com admin --super-admin
---
+++ symfony/README.md
@@ -0,0 +1,4 @@
+symfony
+=======
+
+A Symfony project created on September 4, 2017, 9:45 am.
