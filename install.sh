756c83b55d3f5a9fd59931accc3c662df76c35ce
Cassiano Tartari <cassianotartari@gmail.com>
2017-09-12 07:44:46 -0300

    Instalação com linhas extras para garantir que php está instalado na máquina + correta versão para uso do healthcheck

--- install.sh
+++ install.sh
@@ -5,6 +5,10 @@ apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
 apt-get update
 apt-cache policy docker-engine
 apt-get install -y docker-engine
+curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > ./docker-compose
+mv ./docker-compose /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+apt-get install -y php7.0 php7.0-xml php7.0-zip
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 composer install -d symfony
 docker-compose build
